8486a5502713938799bf65c209ec73af642f5f9c
Q1173419450 <1173419450@qq.com>
2019-08-19 00:32:02 +0800

    test: webpack.base test

--- test/util/webpack-base-test.js
+++ test/util/webpack-base-test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js');
 
-    // console.log(baseConfig);
+    console.log(baseConfig);
     it('entry', () => {
         assert.equal(baseConfig.entry.index.indexOf("baseWebpack/test/smoke/template/src/index/index.js") > -1, true);
         assert.equal(baseConfig.entry.search.indexOf("baseWebpack/test/smoke/template/src/search/index.js") > -1, true);
